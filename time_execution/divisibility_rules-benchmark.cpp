08769f922d0a9cd8d15a8fd057555420de7a81e1
dejvdebil <dav.skrob@email.cz>
2022-11-19 23:34:37 +0100

    measurement of time
    TODO function to measure and make better accuracy

--- time_execution/divisibility_rules-benchmark.cpp
+++ time_execution/divisibility_rules-benchmark.cpp
@@ -1,54 +1,50 @@
-#include<iostream>
-#include<chrono>
+#include <iostream>
+#include <chrono>
 #include <math.h>
 #include <vector>
+#include <random>
 
 using namespace std;
 using namespace std::chrono;
 
-void gen_random_sizes(int size);
-int random_size();
+void gen_random_sample(int sample_size);
+int random_number();
 void divisibility();
 void modulo();
+int find_next_divisor(int maximal_number, int number);
+void erasthotenes_sive();
 
+int MAX_NUMBER = 1000;
+int sample_size = 1000;
 
-int bigest_size = 100;
-int size = 1000;
-
-
-vector<int> random_sizes;
+vector<int> random_numbers;
+vector<int> divisors_modulo;
+vector<int> divisors_div_rules;
+vector<int> primes;
 
 int main(){
-	
-	int time = 0;
-	
-	cout <<"generating random sizes \n";
-	gen_random_sizes(size);
+	cout<<"generating primes up to "<<MAX_NUMBER<<"\n";
+	auto start = high_resolution_clock::now();
+	erasthotenes_sive();
+	auto stop = high_resolution_clock::now();
+	auto time = duration_cast<microseconds>(stop - start);
+	cout << "Time: "<< time.count() << "ms \n";	
+
+	cout <<"generating samples \n";
+	gen_random_sample(sample_size);
 	
 	cout << "testing of divisibility rules \n";
-	
-	//start time
-	auto start = high_resolution_clock::now();
-	
+	auto start1 = high_resolution_clock::now();
 	divisibility();
-	
-	//stop time
-	auto stop = high_resolution_clock::now();
-	
-	auto time1 = duration_cast<microseconds>(stop - start);
+	auto stop1 = high_resolution_clock::now();
+	auto time1 = duration_cast<microseconds>(stop1 - start1);
 	cout << "Time: "<< time1.count() << "ms \n";
-	
+
 	cout << "testing of modulo \n";
-	
-	//start time
-	auto start = high_resolution_clock::now();
-	
+	auto start2 = high_resolution_clock::now();
 	modulo();
-	
-	//stop time
-	auto stop = high_resolution_clock::now();
-	
-	auto time2 = duration_cast<microseconds>(stop - start);
+	auto stop2 = high_resolution_clock::now();
+	auto time2 = duration_cast<microseconds>(stop2 - start2);
 	cout << "Time: "<< time2.count() << "ms \n";
 	
 	//some logic to compere time1 and time2
@@ -57,33 +53,84 @@ int main(){
 	return 0;
 }
 
-void gen_random_sizes(int size){
-	for(int i=0;i<size;i++){
-		random_sizes.push_back(random_size());
+void gen_random_sample(int sample_size){
+	for(int i=0;i<sample_size;i++){
+		random_numbers.push_back(random_number());
 	}
 }
 
-int random_size(){
-	return 10; //remake to return actual random size
+int random_number(){
+	random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type> dist6(1,MAX_NUMBER); // distribution in range [1, 6]
+	return dist6(rng);
 }
 
+
+void erasthotenes_sive(){
+	vector<bool> primenes(MAX_NUMBER, 1);
+	for(int k=0; k<MAX_NUMBER; k++){
+		if(primenes[k]){
+			int num = k+2;
+			primes.push_back(num);
+			for(int f = k+num ;f<MAX_NUMBER; f+=num){
+				primenes[f]=0;
+			}
+		}
+	}
+	/*
+	cout <<"primes:\n";
+	for (auto i: primes) {
+    	cout << i << '\n'; 
+	}
+	*/
+}
 
 //make it as findings of prime divisors?
 // probably yes bc what else shuld it do??
 
 void divisibility(){
-	for(int i=0;i<size;i++){
+	for(int i=0;i<sample_size;i++){
+		int number = random_numbers[i];
+		
 		//divisibility rules implementation
 		//only of prime sizes so... 2;3;5;7;11;13;17;19;23;29
-		cout<<i;
+		
 	}
 }
 
 void modulo(){
-	for(int i=0;i<size;i++){
-		for(int j=1; j<sqrt(bigest_size); j++){
-			//save some sort of the divisors
+
+	if(false){
+	for(int i=0;i<sample_size;i++){
+		int number = random_numbers[i];
+		
+		//broken intended way
+		//find first divisor
+		//divide number by that divisor
+		//find next divisor
+		
+		
+		//optimal way
+		//find divisor
+		//try divide by it until it isnt posible
+		//go to next prime
+		// STONKS
+		
+		//broken
+		while(number < MAX_NUMBER){
+			int divisor = find_next_divisor(1,3);
+			divisors_modulo.push_back(divisor);
+			
 		}
 	}
+	}
 }
-
+//broken
+int find_next_divisor(int maximal_number, int number){
+	for(int j=1; j<sqrt(maximal_number); j++){
+			if(true)
+				return 1;
+			//save the divisors
+		}
+}
--- time_execution/sqrt-benchmark.cpp
+++ time_execution/sqrt-benchmark.cpp
@@ -1,32 +1,91 @@
 #include<iostream>
 #include<chrono>
+#include<vector>
+#include <math.h>
+#include <numeric>
+#include <random>
 
 using namespace std;
+using namespace std::chrono;
+
+//from stack overflow
+double random_range (double const range_min, double const range_max)
+{
+	uniform_real_distribution<double> unif(range_min,range_max);
+	default_random_engine re;
+	return  unif(re);
+  //static unsigned long long const mant_mask53(9007199254740991);
+  //static double const i_to_d53(1.0/9007199254740992.0);
+  //unsigned long long const r( (unsigned long long(rand()) | (unsigned long long(rand()) << 15) | (unsigned long long(rand()) << 30) | (unsigned long long(rand()) << 45)) & mant_mask53 );
+  //return range_min + i_to_d53*double(r)*(range_max-range_min);
+}	//end of stack overflow
+
 
 //void gen_random_numbers(number);
-void divisibility(){};
-void modulo(){};
+double mysqrt(double num){
+	double est = num/2;
+	for(int k; k<6; k++){
+		est = (est/2 + num/(2*est));
+	}
+	return est;
+}
+double fastinvsqrt(double num){
+	return 5;
+}
+
+double realsqrt(double num){
+	return sqrt(num);//zatim...
+	//TODO udelat tady takovou fce, aby jsem si mohl byt jistej ze to je 100% spravne
+}
 
 int main(){
 	int number = 1000;
-	int time = 0;
-	
-	cout <<"generating random numbers \n";
-	//gen_random_numbers(number);
-	
-	cout << "testing of divisibility rules \n";
-	//start time
-	divisibility();
-	//stop time
-	cout << "Time: "<< time << "s \n";
-	
-	cout << "testing of modulo \n";
-	//start time
-	modulo();
-	//stop time
-	cout << "Time: "<< time << "\n";
-	
+
+	auto start = high_resolution_clock::now();
+	for(int i = 0; i<number;i++){
+		double l = mysqrt(i);
+	}
+	auto stop = high_resolution_clock::now();
+	auto time = duration_cast<microseconds>(stop - start);
+	cout << "Time: "<< time.count() << "ms \n";		
+
+	start = high_resolution_clock::now();
+	for(int i = 0; i<number;i++){
+		double l = fastinvsqrt(i);
+	}
+	stop = high_resolution_clock::now();
+	time = duration_cast<microseconds>(stop - start);
+	cout << "Time: "<< time.count() << "ms \n";	
+
+	start = high_resolution_clock::now();
+	for(int i = 0; i<number;i++){
+		double l = sqrt(i);
+	}
+	stop = high_resolution_clock::now();
+	time = duration_cast<microseconds>(stop - start);
+	cout << "Time: "<< time.count() << "ms \n";	
+
+
+	vector<double> m_err;
+	vector<double> f_err;
+	vector<double> std_err;
+	cout << "aquracy test: \n";
+	for(int i = 0; i<number;i++){
+		double randomNumber = random_range(0, 9);
+		double real = realsqrt(randomNumber);
+		double m = mysqrt(randomNumber);
+		m_err.push_back(abs(real-m));
+		double f = fastinvsqrt(randomNumber);
+		f_err.push_back(abs(real-f));
+		double std = sqrt(randomNumber);
+		std_err.push_back(abs(real-std));
+	}
+	double avrg_m_err = accumulate(m_err.begin(), m_err.end(), 0.0) / m_err.size();
+	double avrg_f_err = accumulate(f_err.begin(), f_err.end(), 0.0) / f_err.size();
+	double avrg_std_err = accumulate(std_err.begin(), std_err.end(), 0.0) / std_err.size();
+	cout<<"error of my sqrt is on avrg:"<< avrg_m_err <<"\n";//TODO zlepsit presnost mereni
+	cout<<"error of fast inverse sqrt is on avrg:"<< avrg_f_err<<"\n";
+	cout<<"error of fast inverse sqrt is on avrg:"<< avrg_std_err<<"\n";
+
 	return 0;
 }
-
-
